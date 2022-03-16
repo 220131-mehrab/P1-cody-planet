2f2d43ea1fd06f91b4206f11089f7a96467d5f2b
codyb <cody084@revature.com>
2022-03-16 17:19:40 -0400

    Added SLF4J, jar executable, etc.

---
+++ README.md
@@ -0,0 +1,7 @@
+#Planets
+- Web app that will display different planets to the users as well as inserting other planets to the list
+- They will be search different information on individual planets
+-
+
+#User Stories
+-User can access the landing page of the background page that will greet them.
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,5 +13,16 @@ CREATE TABLE "Info"(
 
     CREATE INDEX "IFK_InfoPlanetsId" ON "Info" ("PlanetId");
 
-    INSERT INTO "Planets" VALUES (1, 'Mercury');
-    INSERT INTO "Planets" VALUES (2, 'Venus');
+    INSERT INTO "planets" VALUES (1, 'Mercury');
+    INSERT INTO "planets" VALUES (2, 'Venus');
+    INSERT INTO "planets" VALUES(3, 'Earth');
+    INSERT INTO "planets" VALUES(4, 'Mars');
+    INSERT INTO "planets" VALUES(5, 'Jupiter');
+    INSERT INTO "planets" VALUES(6, 'Saturn');
+    INSERT INTO "planets" VALUES(7, 'Uranus');
+    INSERT INTO "planets" VALUES(8, 'Neptune');
+
+    INSERT INTO "Info" VALUES(1, 'Information on Mercury', 1);
+    INSERT INTO "Info" VALUES(2, 'Information on Venus', 2);
+    INSERT INTO "Info" VALUES(3, 'Information on Earth', 3);
+    INSERT INTO "Info" VALUES(4, 'Information on Mars', 4);
